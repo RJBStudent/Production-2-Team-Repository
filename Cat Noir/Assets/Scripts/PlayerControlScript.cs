53b9a64ed6582ff4604e0f4c12b9461c5ca683fa
361rjb <361rjb@gmail.com>
2019-01-16 22:06:55 -0500

    First Commit, Player Movement Added Unity Files as well as Player movment in 3D

---
+++ Cat Noir/Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour {
+
+    [SerializeField]
+    private float playerSpeed;
+    float playerX;
+    float playerZ;
+    Transform thisTransform;
+    Rigidbody thisRigidbody;
+
+    
+	// Use this for initialization
+	void Start () {
+        thisTransform = gameObject.transform;
+        thisRigidbody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        UpdateInput();
+        UpdatePosition();
+
+	}
+
+    void UpdateInput()
+    {
+        playerX = Input.GetAxis("Horizontal");
+        playerZ = Input.GetAxis("Vertical");
+
+    }
+    
+    void UpdatePosition()
+    {
+        thisRigidbody.velocity = new Vector3(playerX * playerSpeed, 0, playerZ* playerSpeed);
+    }
+}
