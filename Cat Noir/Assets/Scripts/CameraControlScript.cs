d0ddc0d10b7397f2ed251f6b65c661a9c27b2238
361rjb <361rjb@gmail.com>
2019-01-17 22:31:23 -0500

    Added Camera Movement. Added Camera movement and fixed an issue with unity that would not let me run a project. Was not a code issue it was unity itself.

---
+++ Cat Noir/Assets/Scripts/CameraControlScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlScript : MonoBehaviour {
+
+    
+
+    [SerializeField]
+    private float heightRestrictMax, heightRestrictMin;
+    [SerializeField]
+    private float xSpeed, ySpeed;
+    [SerializeField]
+    private Transform playerPosition;
+    [SerializeField]
+    private float distance;
+
+    float xMovement = 0, yMovement = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetInput();
+        UpdatePosition();
+	}
+
+    void GetInput()
+    {
+        xMovement += Input.GetAxis("Mouse X") * xSpeed;
+        yMovement += Input.GetAxis("Mouse Y") * ySpeed;
+
+        yMovement = Mathf.Clamp(yMovement, heightRestrictMin, heightRestrictMax);
+    }
+
+    void UpdatePosition()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(yMovement, xMovement, 0);
+        transform.position = playerPosition.position + rotation * dir;
+        transform.LookAt(playerPosition.position);
+
+        transform.LookAt(playerPosition);
+    }
+}
