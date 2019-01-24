df8ffb359feac1980b8c6768fd672b65c1307867
361rjb <361rjb@gmail.com>
2019-01-24 00:03:59 -0500

    Added Player Movement With Camera Control Had to use old version of unity due to unusual errors. Add player movement and restricted player control.

---
+++ SheildMan/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    [SerializeField] Transform playerPosition;
+    [SerializeField] float yOffset;
+    [SerializeField] float distance;
+    [SerializeField] float lookAngle;
+
+	// Use this for initialization
+	void Start ()
+    {
+        transform.rotation = Quaternion.Euler(lookAngle, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdatePosition();
+	}
+
+    void UpdatePosition()
+    {
+        transform.position = new Vector3(0,
+            playerPosition.position.y + yOffset, playerPosition.position.z + distance);
+
+        
+    }
+}
---
+++ SheildMan/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] float playerSpeed;
+    [SerializeField] float acceleration;
+
+    CharacterController thisPlayerControl;
+
+    float xDirection, zDirection;
+    Vector3 velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        thisPlayerControl = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GetInput();
+        MovePlayer();
+
+    }
+
+    void GetInput()
+    {
+        xDirection = Input.GetAxisRaw("Horizontal");
+        zDirection = Input.GetAxisRaw("Vertical");
+    }
+
+    void MovePlayer()
+    {
+        Vector3 dir = new Vector3(xDirection, 0, zDirection);
+
+        velocity = Vector3.Lerp(velocity, dir*playerSpeed, acceleration*Time.deltaTime);
+
+        thisPlayerControl.Move(velocity* Time.deltaTime);
+
+    }
+}
