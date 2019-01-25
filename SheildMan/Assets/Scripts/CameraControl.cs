09136652b6236454e26b69a9d83ccaa7125179db
361rjb <361rjb@gmail.com>
2019-01-25 11:34:34 -0500

    Added a camera lerp Camera doesn't jolt to player position.

--- SheildMan/Assets/Scripts/CameraControl.cs
+++ SheildMan/Assets/Scripts/CameraControl.cs
@@ -8,12 +8,19 @@ public class CameraControl : MonoBehaviour {
     [SerializeField] float yOffset;
     [SerializeField] float distance;
     [SerializeField] float lookAngle;
+    [SerializeField] float lerpSpeed;
+
+    Vector3 pos, newPos;
 
 	// Use this for initialization
 	void Start ()
     {
         transform.rotation = Quaternion.Euler(lookAngle, 0, 0);
-	}
+
+        pos = new Vector3(0, playerPosition.position.y + yOffset,
+            playerPosition.position.z + distance);
+        transform.position = pos;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -23,9 +30,9 @@ public class CameraControl : MonoBehaviour {
 
     void UpdatePosition()
     {
-        transform.position = new Vector3(0,
-            playerPosition.position.y + yOffset, playerPosition.position.z + distance);
-
-        
+         newPos = new Vector3(0, playerPosition.position.y + yOffset,
+             playerPosition.position.z + distance);
+        pos = Vector3.Lerp(pos, newPos, Time.deltaTime * lerpSpeed);
+        transform.position = pos;
     }
 }
