291c0ee3a8415ab49e88bec351e2c4abfb475ca8
361rjb <361rjb@gmail.com>
2019-01-26 12:04:24 -0500

    Added Projectile Scriptable object Added movement for bullets and a easier way to code them

---
+++ SheildMan/Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Projectile")]
+public class Projectile : ScriptableObject {
+
+    [SerializeField] public float speed;
+    [SerializeField] public float acceleration;
+    [SerializeField] public float angle;
+
+
+
+}
---
+++ SheildMan/Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+
+    [SerializeField] Projectile proj;
+    [SerializeField] Transform playerTransform;
+
+    Vector3 dir;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 dir = new Vector3(transform.position.x+Mathf.Cos(Mathf.Deg2Rad * proj.angle), transform.position.y, transform.position.z+Mathf.Sin(Mathf.Deg2Rad * proj.angle) );
+        transform.position = Vector3.Lerp(transform.position, dir * proj.speed, proj.acceleration * Time.deltaTime);
+        checkBounds();
+		
+	}
+
+    void checkBounds()
+    {
+
+        if ((transform.position.x < playerTransform.position.x - 10 || transform.position.x > playerTransform.position.x + 10)
+            || (transform.position.z < playerTransform.position.z - 20 || transform.position.z > playerTransform.position.z + 100))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
