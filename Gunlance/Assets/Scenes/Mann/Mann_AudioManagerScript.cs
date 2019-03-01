cea35a066639e2b3d1d2c4b0f56895f5a2dd585c
TommyMann <thomas.mann@mymail.champlain.edu>
2019-03-01 11:07:48 -0500

    Added Sliding Mechanic and Audio Stuff
    Sliding mechanic is in Mann_PlayerMovement, public variables are there for designer modification. Audio Stuff in place, is a singleton that can be called from anywhere

---
+++ Gunlance/Assets/Scenes/Mann/Mann_AudioManagerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mann_AudioManagerScript : MonoBehaviour {
+
+   public static Mann_AudioManagerScript instance;
+
+   AudioSource[] sounds;
+   void Awake()
+   {
+      //Check if instance already exists
+      if (instance == null)
+
+         //if not, set instance to this
+         instance = this;
+
+      //If instance already exists and it's not this:
+      else if (instance != this)
+
+         //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+         Destroy(gameObject);
+
+      //Sets this to not be destroyed when reloading scene
+      DontDestroyOnLoad(gameObject);
+
+      sounds = Resources.FindObjectsOfTypeAll<AudioSource>();
+   }
+
+   // Use this for initialization
+   void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   public void PlaySound(string clipName)
+   {
+      for(int i = 0; i < sounds.Length; i++)
+      {
+         if(sounds[i].clip.name == clipName)
+         {
+            sounds[i].Play();
+         }
+      }
+   }
+
+}
