e7dbac67cb0723b60a02b73c8cf85c747b9b1d12
David Carey <david.carey@mymail.champlain.edu>
2019-03-02 13:24:47 -0500

    adding my level + tween

---
+++ Gunlance/Assets/DavidScreenShakeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DavidScreenShakeScript : MonoBehaviour {
+
+    Camera cam;
+
+
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<Camera>();
+        StartCoroutine(ShakeScreen());
+	}
+	
+	// Update is called once per frame
+	IEnumerator ShakeScreen() {
+
+        yield return new WaitForSeconds(1);
+        cam.DOShakePosition(1,3,10,90);
+
+	}
+}
