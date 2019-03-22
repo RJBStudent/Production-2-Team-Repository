0a67bfdf2955f8e3c41289441809a579bb5f854c
CHAMPLAIN\jacob.ettinger <jacob.ettinger@mymail.champlain.edu>
2019-03-22 12:33:34 -0400

    Editing scene

--- Gunlance/Assets/Scripts/PlayerMovementWithAudioScript.cs
+++ Gunlance/Assets/Scripts/PlayerMovementWithAudioScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementScriptAudioScript : MonoBehaviour
+public class PlayerMovementWithAudioScript : MonoBehaviour
 {
 
 
