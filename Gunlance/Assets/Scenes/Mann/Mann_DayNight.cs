17d55ed05625262e921d825259d2de3f8d46fa16
TommyMann <thomas.mann@mymail.champlain.edu>
2019-03-30 14:05:41 -0400

    Added Main Menu and Pause Menu
    All Changes are in the Mann Folder

---
+++ Gunlance/Assets/Scenes/Mann/Mann_DayNight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mann_DayNight : MonoBehaviour
+{
+   // Use this for initialization
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+      //Rotate Sun with Speed
+      transform.RotateAround(Vector3.zero, Vector3.right, 10f * Time.deltaTime);
+      transform.LookAt(Vector3.zero);
+
+   }
+}
---
+++ Gunlance/Assets/Scenes/Mann/Mann_GUImanager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mann_GUImanager : MonoBehaviour
+{
+   public static bool gamePaused = false;
+
+   public GameObject pauseMenuUI;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if(Input.GetButtonDown("Pause"))
+      {
+         if(gamePaused)
+         {
+            Resume();
+         }
+         else
+         {
+            Pause();
+         }
+      }
+   }
+
+   public void Resume()
+   {
+      pauseMenuUI.SetActive(false);
+      gamePaused = false;
+      Time.timeScale = 1;
+   }
+
+   public void Pause()
+   {
+      pauseMenuUI.SetActive(true);
+      gamePaused = true;
+      Time.timeScale = 0;
+   }
+   
+   public void Options()
+   {
+      Debug.Log("Options... ");
+   }
+
+   public void Quit()
+   {
+      Debug.Log("Quitting... "); 
+   }
+}
