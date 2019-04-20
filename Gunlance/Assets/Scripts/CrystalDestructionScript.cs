1558a67021160d7704f3386780a6c9dd3fbaee03
Rose Gruner <rose.gruner@mymail.champlain.edu>
2019-04-20 16:24:40 -0400

    Added broken crystal asset and began crystal particle effect

---
+++ Gunlance/Assets/Scripts/CrystalDestructionScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalDestructionScript : MonoBehaviour
+{
+    [SerializeField] GameObject crystalShards;
+    [SerializeField] GameObject particleBurst;
+
+    public void OnExplode()
+    {
+        GameObject shards = Instantiate(crystalShards);
+    }
+}
