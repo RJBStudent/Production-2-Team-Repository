9fcc5bd26126af8d3ab152ae8c8692769ac79176
TommyMann <thomas.mann@mymail.champlain.edu>
2019-04-22 11:58:13 -0400

    Revert "Finished crystal explosion effect a few other effect tweaks"
    This reverts commit 1f1c69649bac14613295bb7229b41aceb6dd3c65.

--- Gunlance/Assets/Scripts/CrystalDestructionScript.cs
+++ Gunlance/Assets/Scripts/CrystalDestructionScript.cs
@@ -1,49 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 public class CrystalDestructionScript : MonoBehaviour
 {
     [SerializeField] GameObject crystalShards;
     [SerializeField] GameObject particleBurst;
-	 [SerializeField] Light exLight;
 	 [SerializeField] float shardLifetime;
 
 	 [SerializeField] float exPower;
-	 [SerializeField] Transform exPoint;
 	 [SerializeField] float exRadius;
 	 Vector3 exPos;
-
-	 [SerializeField] float lightFade;
 
 	 public void OnExplode()
     {
         GameObject shards = Instantiate(crystalShards);
 		  shards.transform.position = transform.position;
 		  shards.transform.rotation = transform.rotation;
-
 		  GameObject burst = Instantiate(particleBurst);
 		  burst.transform.position = transform.position;
 		  burst.transform.rotation = transform.rotation;
 
-		  Light light = Instantiate(exLight);
-		  light.transform.position = exPoint.position;
-		  light.DOIntensity(0, lightFade);
-
-		  foreach (Transform child in shards.transform)
+		  foreach (Transform child in crystalShards.transform)
 		  {
 				Rigidbody rb = child.gameObject.GetComponent<Rigidbody>();
 
 				if (rb != null)
 				{
-					rb.AddExplosionForce(exPower, exPoint.transform.position, exRadius, 0, ForceMode.VelocityChange);
+
+					rb.AddExplosionForce(exPower, transform.position, exRadius, 1, ForceMode.VelocityChange);
+
+					rb.useGravity = true;
 				}
 		  }
 
 		  Destroy(gameObject);
-		  Destroy(burst, shardLifetime);
+		  Destroy(burst, .4f);
 		  Destroy(shards, shardLifetime);
-		  Destroy(light, lightFade + .2f);
     }
 }
