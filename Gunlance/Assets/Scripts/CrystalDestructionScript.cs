7fe4143be52894d4d23c2ffc44dc6deeb08c4ba3
TommyMann <thomas.mann@mymail.champlain.edu>
2019-04-22 12:03:17 -0400

    Revert "Added particles to crystal destroy couldn't get shards working properly"
    This reverts commit 652c8427a36395d7a35a2653c056fe76a9a8c20d.

--- Gunlance/Assets/Scripts/CrystalDestructionScript.cs
+++ Gunlance/Assets/Scripts/CrystalDestructionScript.cs
@@ -6,36 +6,9 @@ public class CrystalDestructionScript : MonoBehaviour
 {
     [SerializeField] GameObject crystalShards;
     [SerializeField] GameObject particleBurst;
-	 [SerializeField] float shardLifetime;
 
-	 [SerializeField] float exPower;
-	 [SerializeField] float exRadius;
-	 Vector3 exPos;
-
-	 public void OnExplode()
+    public void OnExplode()
     {
         GameObject shards = Instantiate(crystalShards);
-		  shards.transform.position = transform.position;
-		  shards.transform.rotation = transform.rotation;
-		  GameObject burst = Instantiate(particleBurst);
-		  burst.transform.position = transform.position;
-		  burst.transform.rotation = transform.rotation;
-
-		  foreach (Transform child in crystalShards.transform)
-		  {
-				Rigidbody rb = child.gameObject.GetComponent<Rigidbody>();
-
-				if (rb != null)
-				{
-
-					rb.AddExplosionForce(exPower, transform.position, exRadius, 1, ForceMode.VelocityChange);
-
-					rb.useGravity = true;
-				}
-		  }
-
-		  Destroy(gameObject);
-		  Destroy(burst, .4f);
-		  Destroy(shards, shardLifetime);
     }
 }
