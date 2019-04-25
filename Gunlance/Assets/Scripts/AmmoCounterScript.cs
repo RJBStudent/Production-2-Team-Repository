b329cbf3508c380dd081606cb7a5f1c39f5d587e
TommyMann <thomas.mann@mymail.champlain.edu>
2019-04-25 17:53:14 -0400

    Added mural audio and recharge audio

--- Gunlance/Assets/Scripts/AmmoCounterScript.cs
+++ Gunlance/Assets/Scripts/AmmoCounterScript.cs
@@ -5,56 +5,57 @@ using DG.Tweening;
 
 public class AmmoCounterScript : MonoBehaviour
 {
-    Material cubeMat;
-    [SerializeField] Gradient crystalColors;
-    [SerializeField] float rotSpeed;
-    [SerializeField] GameObject crystalFlash;
-	 [SerializeField] GameObject flashNewCharge;
+	Material cubeMat;
+	[SerializeField] Gradient crystalColors;
+	[SerializeField] float rotSpeed;
+	[SerializeField] GameObject crystalFlash;
+	[SerializeField] GameObject flashNewCharge;
 
-	 PlayerMovementScript movementScript;
-	 int lastCharge;
+	PlayerMovementScript movementScript;
+	int lastCharge;
 
 	private void Start()
-    {
-		  cubeMat = GetComponent<Renderer>().material;
+	{
+		cubeMat = GetComponent<Renderer>().material;
 
-		  movementScript = GetComponentInParent<PlayerMovementScript>();
-    }
+		movementScript = GetComponentInParent<PlayerMovementScript>();
+	}
 
-    void Update()
-    {
-		  CrystalUpdate();
-    }
+	void Update()
+	{
+		CrystalUpdate();
+	}
 
-    void CrystalUpdate()
-    {
-        //update crystal color to charge level
-        cubeMat.color = crystalColors.Evaluate(movementScript.charge / movementScript.maxShots);
-        cubeMat.SetColor("_EmissionColor", crystalColors.Evaluate(movementScript.charge / movementScript.maxShots));
+	void CrystalUpdate()
+	{
+		//update crystal color to charge level
+		cubeMat.color = crystalColors.Evaluate(movementScript.charge / movementScript.maxShots);
+		cubeMat.SetColor("_EmissionColor", crystalColors.Evaluate(movementScript.charge / movementScript.maxShots));
 
-        transform.Rotate(0, -movementScript.charge * rotSpeed, 0);
+		transform.Rotate(0, -movementScript.charge * rotSpeed, 0);
 
-		  var chargeInt = Mathf.FloorToInt(movementScript.charge);
+		var chargeInt = Mathf.FloorToInt(movementScript.charge);
 
-		  if (chargeInt > lastCharge)
-		  {
-				NewShot();
-		  }
-		  lastCharge = Mathf.FloorToInt(movementScript.charge);
-    }
+		if (chargeInt > lastCharge)
+		{
+			NewShot();
+			Mann_AudioManagerScript.instance.PlayInterruptedSound("CrystalChargedFull");
+		}
+		lastCharge = Mathf.FloorToInt(movementScript.charge);
+	}
 
-	 //flash crystal when reaching new shot charge
-	 void NewShot()
-	 {
-		  GameObject flash = Instantiate(flashNewCharge, transform);
+	//flash crystal when reaching new shot charge
+	void NewShot()
+	{
+		GameObject flash = Instantiate(flashNewCharge, transform);
 		Debug.Log(flash.transform.position);
-		  Destroy(flash, .7f);
-	 }
+		Destroy(flash, .7f);
+	}
 
-	 //flash crystal on firing
-	 public void Shoot()
-    {
-		  GameObject flash = Instantiate(crystalFlash, transform);
-        Destroy(flash, .7f);
-    }
+	//flash crystal on firing
+	public void Shoot()
+	{
+		GameObject flash = Instantiate(crystalFlash, transform);
+		Destroy(flash, .7f);
+	}
 }
--- Gunlance/Assets/Scripts/MuralInteractScript.cs
+++ Gunlance/Assets/Scripts/MuralInteractScript.cs
@@ -4,61 +4,68 @@ using UnityEngine;
 
 public class MuralInteractScript : MonoBehaviour
 {
-	 [SerializeField] GameObject muralReveal;
-	 [SerializeField] GameObject prompt;
-	 bool revealed = false;
-	 bool close = false;
-	 bool showing = false;
+	[SerializeField] GameObject muralReveal;
+	[SerializeField] GameObject prompt;
+	bool revealed = false;
+	bool close = false;
+	bool showing = false;
+	bool soundPlayed = false;
 
-	 private void Awake()
-	 {
-		  muralReveal.SetActive(false);
-		  prompt.SetActive(false);
-	 }
+	private void Awake()
+	{
+		muralReveal.SetActive(false);
+		prompt.SetActive(false);
+	}
 
-	 private void OnTriggerStay(Collider other)
-	 {
-		  if (!revealed && other.CompareTag("Player"))
-		  {
-		      close = true;
-			   if (showing)
-			   {
-					other.gameObject.GetComponent<PlayerMovementScript>().canJump = false;
-			   }
-		  }
-		  else if (revealed)
-		  {
-				prompt.SetActive(false);
-		  }
-	 }
+	private void OnTriggerStay(Collider other)
+	{
+		if (!revealed && other.CompareTag("Player"))
+		{
+			close = true;
+			if (showing)
+			{
+				other.gameObject.GetComponent<PlayerMovementScript>().canJump = false;
+			}
+		}
+		else if (revealed)
+		{
+			prompt.SetActive(false);
+		}
+	}
 
-	 private void OnTriggerExit(Collider other)
-	 {
-	     if (other.CompareTag("Player"))
-		  {
-				close = false;
-			   other.gameObject.GetComponent<PlayerMovementScript>().canJump = true;
-		  }
-	 }
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			close = false;
+			other.gameObject.GetComponent<PlayerMovementScript>().canJump = true;
+		}
+	}
 
-	 private void Update()
-	 {
-		  if (close && !showing)
-		  {
-				prompt.SetActive(true);
-				showing = true;
-		  }
-		  else if (!close && showing)
-		  {
-				prompt.SetActive(false);
-				showing = false;
-		  }
+	private void Update()
+	{
+		if (close && !showing)
+		{
+			prompt.SetActive(true);
+			showing = true;
+		}
+		else if (!close && showing)
+		{
+			prompt.SetActive(false);
+			showing = false;
+		}
 
-		  //prompt displayed and A pressed
-		  if (showing && Input.GetButtonDown("Jump"))
-		  {
-				muralReveal.SetActive(true);
-				revealed = true;
-		  }
-	 }
+		//prompt displayed and A pressed
+		if (showing && Input.GetButtonDown("Jump"))
+		{
+			muralReveal.SetActive(true);
+			revealed = true;
+
+			if (soundPlayed == false)
+			{
+				Mann_AudioManagerScript.instance.PlaySound("MuralReveal");
+				soundPlayed = true;
+			}
+		}
+	}
 }
